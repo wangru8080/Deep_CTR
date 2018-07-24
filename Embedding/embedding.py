2b9cae93b7f55430e4dda9b288c874f4db0904b0
wangru <wangru8080@163.com>
2018-07-24 15:09:05 +0800

    Update embedding.py

--- Embedding/embedding.py
+++ Embedding/embedding.py
@@ -18,45 +18,6 @@ class Embedding:
         self.embedding_size = embedding_size
         self.random_seed = random_seed
 
-    def FeatureDictionary(self, train, test):
-        '''
-        只对离散特征进行编号
-        1.离散特征，one-hot之后每一列都是一个新的特征维度(计算编号时，不算0)。所以，原来的一维度对应的是很多维度，编号也是不同的。
-        2.利用category_dict来存储索引信息，每列特征对应一个dict，这个dict存储不同取值对应的编号信息。方便后续进行embedding
-        '''
-
-        df = pd.concat([train, test], axis=0)
-        cate_dict = {}
-        total_cnt = 0
-
-        for col in df.columns:
-            if col in self.category_feature:
-                unique_cnt = df[col].nunique()
-                unique_vals = df[col].unique()
-                cate_dict[col] = dict(zip(unique_vals, range(total_cnt, total_cnt + unique_cnt)))
-                total_cnt = total_cnt + unique_cnt
-
-        self.category_dict = cate_dict
-        self.category_size = total_cnt
-        print('category_dict=', cate_dict)
-        print('=' * 20)
-        print('category_size=', total_cnt)
-
-    def parse(self, df):
-        df_category_index = df[self.category_feature]
-        for col in self.category_feature:
-            df_category_index[col] = df_category_index[col].map(self.category_dict[col])
-
-        category_index = df_category_index.values.tolist()
-        del df_category_index
-        gc.collect()
-
-        self.category_field_size = len(category_index[0])
-        print('category_index=', category_index)
-        print('=' * 20)
-        print('category_field_size=', self.category_field_size)
-        return category_index
-
     def to_embedding_vector(self, category_index, df, isPrintEmbeddingInfo=False):
         tf.set_random_seed(self.random_seed)
 
@@ -96,6 +57,9 @@ if __name__ == '__main__':
     continuous_feature = ['feat_num_1', 'feat_num_2']
 
     embedding = Embedding(ignore_feature=ignore_feature, category_feature=category_feature, continuous_feature=continuous_feature)
-    embedding.FeatureDictionary(train, test)
-    category_index = embedding.parse(train)
+    
+    dataParse = DataParse(continuous_feature=continuous_feature, category_feature=category_feature)
+    dataParse.FeatureDictionary(train, test)
+    category_index = dataParse.parse(train)
+    
     embedding.to_embedding_vector(category_index, train, isPrintEmbeddingInfo=True)
