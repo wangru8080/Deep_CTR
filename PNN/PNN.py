af94fdc964f4c6580828eeba70632b6fb869daa6
wangru <wangru8080@163.com>
2018-07-30 11:24:29 +0800

    Update PNN.py

--- PNN/PNN.py
+++ PNN/PNN.py
@@ -70,9 +70,9 @@ class PNN(BaseEstimator, TransformerMixin):
 
             with tf.name_scope('Embedding_Layer'):
                 weights['feature_embeddings'] = tf.Variable(tf.random_normal([self.feature_size, self.embedding_size], 0.0, 0.01), name='feature_embeddings')
-                self.embeddings = tf.nn.embedding_lookup(weights['feature_embeddings'], self.feature_index)  # [None, field_size, 1]
+                self.embeddings = tf.nn.embedding_lookup(weights['feature_embeddings'], self.feature_index)  # [None, field_size, embedding_size]
                 feat_value = tf.reshape(self.feature_value, shape=[-1, self.field_size, 1])  # [None, field_size, 1]
-                self.embeddings = tf.multiply(self.embeddings, feat_value)  # [None, field_size, 1]
+                self.embeddings = tf.multiply(self.embeddings, feat_value)  # [None, field_size, embedding_size]
 
             with tf.name_scope('Product_Layer'):
                 if self.use_inner:
