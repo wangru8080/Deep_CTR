d0fcbee4110bd6652f9b1ae5ec3632bc0d768ccf
wangru <wangru8080@163.com>
2018-07-30 11:23:05 +0800

    Update DeepFM.py

--- DeepFM/DeepFM.py
+++ DeepFM/DeepFM.py
@@ -70,9 +70,9 @@ class DeepFM(BaseEstimator, TransformerMixin):
 
             with tf.name_scope('init'):
                 weights['feature_embeddings'] = tf.Variable(tf.random_normal([self.feature_size, self.embedding_size], 0.0, 0.01), name='feature_embeddings')
-                self.embeddings = tf.nn.embedding_lookup(weights['feature_embeddings'], self.feature_index)  # [None, field_size, 1]
+                self.embeddings = tf.nn.embedding_lookup(weights['feature_embeddings'], self.feature_index)  # [None, field_size, embedding_size]
                 feat_value = tf.reshape(self.feature_value, shape=[-1, self.field_size, 1])  # [None, field_size, 1]
-                self.embeddings = tf.multiply(self.embeddings, feat_value)  # [None, field_size, 1]
+                self.embeddings = tf.multiply(self.embeddings, feat_value)  # [None, field_size, embedding_size]
 
             with tf.name_scope('fm_part'):
                 # FM_first_order
